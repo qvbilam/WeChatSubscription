bb6a5018247cfc42ff69b0c479018ac7df650865
guoyi <534511019@qq.com>
2018-10-10 16:33:40 +0800

    msg test

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 class MessageController extends Controller
 {
     //
-//    public function
+    public function responseMsg(Request $request)
+    {
+        echo 123;
+    }
 }
