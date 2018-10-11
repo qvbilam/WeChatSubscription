62defdde5e225c314e79d30853664ff98e772b0c
guoyi <534511019@qq.com>
2018-10-11 10:40:10 +0800

    msg log

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Model\UserMsg;
+use Illuminate\Support\Facades\Log;
 
 class MessageController extends Controller
 {
--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Redis;
 use App\Http\Controllers\TokenController;
 use App\Http\Controllers\ButtonController;
 use App\Http\Controllers\MessageController;
+use Illuminate\Support\Facades\Log;
 
 class WeChatController extends Controller
 {
@@ -25,6 +26,7 @@ class WeChatController extends Controller
         //获取微信传来的消息
         $postStr = file_get_contents('php://input');
         if (!$postStr) {
+            Log::info($postStr);
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
             $fromUsername = $postObj->FromUserName;
             $toUsername = $postObj->ToUserName;
