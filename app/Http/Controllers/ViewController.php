cbd6ef6fe73b8e29a42afcfa65265b8897a03357
guoyi <534511019@qq.com>
2018-10-12 10:54:19 +0800

    get code

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Ixudra\Curl\Facades\Curl;
 
 class ViewController extends Controller
 {
     //普通司机注册
+    static public $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx9e40a29d39faba87&redirect_uri=";
+    static public $parameter = "&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
+
     public function register()
     {
-        return view('welcome');
+        $web = json_encode(env('TEST_WECHAT_WEB_ME') . "api/register");
+        $response = Curl::to(self::$url . $web . self::$parameter)
+            ->get();
+        dd($response);
+        //return view('welcome');
     }
 
     //司机提现
