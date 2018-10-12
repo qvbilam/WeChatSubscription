308d655c13c1f6f218fc8c36ec5d80c102924037
guoyi <534511019@qq.com>
2018-10-12 13:43:20 +0800

    view openid wev

--- routes/api.php
+++ routes/api.php
@@ -17,33 +17,37 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::any('valid', 'TokenController@valid'); //验证服务器token
-Route::any('getToken', 'TokenController@getToken'); //获取token验证
-Route::any('index', 'WeChatController@index'); //首页
+Route::any('valid', 'TokenController@valid');                               //验证服务器token
+Route::any('getToken', 'TokenController@getToken');                         //获取token验证
+Route::any('index', 'WeChatController@index');                              //首页
 
 
 /*  button按钮  */
-Route::any('createButton', 'ButtonController@createButton'); //创建button
-Route::any('deleteButton', 'ButtonController@deleteButton'); //删除Button
-Route::any('getButton', 'ButtonController@getButton'); //获取button
+Route::any('createButton', 'ButtonController@createButton');                //创建button
+Route::any('deleteButton', 'ButtonController@deleteButton');                //删除Button
+Route::any('getButton', 'ButtonController@getButton');                      //获取button
 
 
 /*消息*/
-Route::any('responseMsg', 'MessageController@responseMsg'); //被动回复消息
+Route::any('responseMsg', 'MessageController@responseMsg');                 //被动回复消息
 
 /*获取用户信息*/
-Route::any('getUserInfo', 'WeChatUserController@getUserInfo'); //获取单个用户信息
-Route::any('getQrCode', 'WeChatUserController@getQrCode'); //获取单个用户信息
+Route::any('getUserInfo', 'WeChatUserController@getUserInfo');              //获取单个用户信息
+Route::any('getQrCode', 'WeChatUserController@getQrCode');                  //获取单个用户信息
 
 /*页面*/
-Route::any('register', 'ViewController@register'); //司机注册 ---- 普通
-Route::any('withdrawMoney', 'ViewController@withdrawMoney'); //司机提现
-Route::any('getOrder', 'ViewController@getOrder'); //获取司机订单
-Route::any('bind', 'ViewController@bind'); //司机绑定
-Route::any('macRepair', 'ViewController@macRepair'); //设备报修
+Route::any('register', 'ViewController@register');                          //司机注册 ---- 普通
+Route::any('withdrawMoney', 'ViewController@withdrawMoney');                //司机提现
+Route::any('getOrder', 'ViewController@getOrder');                          //获取司机订单
+Route::any('bind', 'ViewController@bind');                                  //司机绑定
+Route::any('macRepair', 'ViewController@macRepair');                        //设备报修
 /*页面重定向*/
-Route::any('getOpenId', 'ViewController@getOpenId'); //获取openid
-Route::any('registerList', 'ViewController@registerList'); //司机注册 ---- 普通
+Route::any('getOpenId', 'ViewController@getOpenId');                        //获取openid
+Route::any('registerList', 'ViewController@registerList');                  //司机注册 ---- 普通
+Route::any('withdrawMoneyList', 'ViewController@withdrawMoneyList');        //司机提现
+Route::any('getOrderList', 'ViewController@getOrderList');                  //获取司机订单
+Route::any('bindList', 'ViewController@bindList');                          //司机绑定
+Route::any('macRepairList', 'macRepairList@bindList');                      //设备报修
 
 
 
