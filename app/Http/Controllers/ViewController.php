66d095fc96b3cf29cd65b6717129b1dc3a0feb24
guoyi <534511019@qq.com>
2018-10-12 11:18:56 +0800

    get open did

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -11,13 +11,28 @@ class ViewController extends Controller
     static public $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx9e40a29d39faba87&redirect_uri=";
     static public $parameter = "&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
 
-    public function register()
+    public function register(Request $request)
     {
-        $web = json_encode(env('TEST_WECHAT_WEB_ME') . "api/register");
-        $response = Curl::to(self::$url . $web . self::$parameter)
-            ->get();
-        dd($response);
-        //return view('welcome');
+        $REDIRECT_URI = env('TEST_WECHAT_WEB_ME') . '/api/getOpenId';
+        $scope = 'snsapi_base';
+        $state = 'TEST';
+        $to_url = self::$url . urlencode($REDIRECT_URI) . '&response_type=code&scope=' . $scope . '&state=' . $state . '#wechat_redirect';
+        header("Location:" . $to_url);
+    }
+
+    public function getOpenId()
+    {
+        $APPID='wx9e40a29d39faba87';
+        $SECRET='23bdd2ef6129a278a3af4e57992105b4';
+        $state='TEST';
+        $code='';
+        if($_GET['state']==$state) {
+            $code = $_GET['code'];
+            $uinfo = file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=" . $APPID . "&secret=" . $SECRET . "&code={$code}&grant_type=authorization_code");
+            $uinfo = (array)json_decode($uinfo);
+            $openid = $uinfo['openid'];
+            echo "OPENID IS: " . $openid;
+        }
     }
 
     //司机提现
