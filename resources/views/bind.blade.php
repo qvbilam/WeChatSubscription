aaa9292ce6144aa01e113cce16697363fe21aad2
guoyi <534511019@qq.com>
2018-10-17 10:43:29 +0800

    bind

--- resources/views/bind.blade.php
+++ resources/views/bind.blade.php
@@ -3,7 +3,7 @@
     <h1>服务号绑定</h1>
     <h4>温馨提示:绑定服务号时，需要填写"司机注册"时的手机号,如未注册请先注册</h4>
     <input type="text" id="phone">
-    <input style="display: none;" type="text" id="openId" value="{{ $openId }}}">
+    <input style="display: none;" type="text" id="openId" value="{{ $openId }}">
     <button id="bind">绑定</button>
 </bod>
 </html>
