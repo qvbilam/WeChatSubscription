314f470ceb5575181f529159382654fefd916e35
guoyi <534511019@qq.com>
2018-10-22 12:14:32 +0800

    withdraw

--- resources/views/withdraw.blade.php
+++ resources/views/withdraw.blade.php
@@ -3,7 +3,7 @@
 <h1>司机提现</h1>
 <h3>温馨提示:提现金额大于等于100元,每周三可提现</h3>
 <input style="display: none;" type="text" id="openId" value="{{ $openId }}">
-可提现金额：<input type="text" id="money" value="{{ $money }}">
+可提现金额：<input type="text" id="money" value="{{ $money / 100 }}">
 
 <button id="submit">提现</button>
 
