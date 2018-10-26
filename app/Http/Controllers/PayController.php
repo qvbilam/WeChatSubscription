ef412a96aaf50709021b8dc3df3cf720351bd554
guoyi <534511019@qq.com>
2018-10-26 11:18:43 +0800

    putForward

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -156,7 +156,7 @@ class PayController extends Controller
     public function putForward($money,$openId,$tradeno)
     {
         $url = "https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers";
-        $appId = env('MINI_APP_ID');
+        $appId = env('TEST_WECHAT_APPID');
         $mchId = env('MCHID');
         $nonce_str = WxpayController::getNonceStr();
         //$sign_type = 'MD5';
